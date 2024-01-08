274b37d65a9ff837f55759fa1098970583776622
Avic0405 <147046252+Avic0405@users.noreply.github.com>
2024-01-08 15:41:40 +0530

    Update javaMain.java

--- javaMain.java
+++ javaMain.java
@@ -8,35 +8,26 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 public class GmailComposeAutomation {
     public static void main(String[] args) {
-        // Sender's Gmail account credentials
         final String username = "email";
         final String password = "password";
-        // email address of whos u want to share
         String toEmail = "email";
-        // Sender's email properties
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-        // Create a session with authentication
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, password);
             }
         });
         try {
-            // Create a default MimeMessage object
+            
             Message message = new MimeMessage(session);
-            // Set From: header field
             message.setFrom(new InternetAddress(username));
-            // Set To: header field
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
-            // Set Subject: header field
-            message.setSubject("Testing Gmail Automation");
-            // Set the actual message
-            message.setText("Hello, this is a test email sent through JavaMail API.");
-            // Send the message
+            message.setSubject("incubyte");
+            message.setText("QA test for incubyte.");
             Transport.send(message);
             System.out.println("Email sent successfully!");
         } catch (MessagingException e) {
